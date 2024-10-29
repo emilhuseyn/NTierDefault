b1c50f6e3d1db4115fba73cd1509dcb2c7fd7f6b
emilhuseyn <emiilhuseynov@23gmail.com>
2024-10-29 21:47:30 +0400

    for my students

---
+++ API-NTierArch-main/App/src/App.API/Controllers/Commons/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers.Commons
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase { }
+}
